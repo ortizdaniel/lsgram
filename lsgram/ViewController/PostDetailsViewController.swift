8994d9f2bb3ab5f673aa0fe241a47116bdac78e9
Manel Manchón Gascó <Manel@iMac-de-Nuria.local>
2018-12-28 16:22:45 +0100

    se ha añadido la direccion a las busquedas de localizaciones

--- lsgram/ViewController/PostDetailsViewController.swift
+++ lsgram/ViewController/PostDetailsViewController.swift
@@ -91,7 +91,7 @@ class PostDetailsViewController : UIViewController, UITableViewDelegate, UITable
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell")!
         let selectedItem = matchingItems[indexPath.row].placemark
         cell.textLabel?.text = selectedItem.name
-        cell.detailTextLabel?.text = ""
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
         
         return cell
     }
@@ -115,6 +115,30 @@ class PostDetailsViewController : UIViewController, UITableViewDelegate, UITable
                 self.tableView.reloadData()
             }
         })
+    }
+    
+    func parseAddress(selectedItem: MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
     }
     
 }
